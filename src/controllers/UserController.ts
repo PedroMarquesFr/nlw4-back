16751a9ccefcc71e042394485b9a4331fa435456
Pedro MF <pedromarques1011@gmail.com>
2021-02-24 10:43:24 -0300

    feito survey logica

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,28 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
   async create(request: Request, response: Response) {
-    const body = request.body;
-    console.log(body);
-    return response.send();
+    const { name, email } = request.body;
+
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userAlreadyExists = await usersRepository.findOne({
+        email
+    })
+
+    if(userAlreadyExists){
+        return response.status(400).json({
+          error:"User already exists!"  
+        })
+    }
+
+    //voce realmente precisa criar, nda pra passar direto no usersRepository
+    const user = usersRepository.create({ name, email });
+
+    await usersRepository.save(user);
+    return response.json(user);
   }
 }
 
